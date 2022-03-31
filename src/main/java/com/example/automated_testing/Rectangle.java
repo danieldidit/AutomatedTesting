a746e35b2c31bd28d4b26c5fe814efcd3d6926f1
Daniel Humphreys <danielhumphreys@live.ca>
2022-03-31 12:58:46 -0400

    Updated the RectangleTest.java and Rectangle.java to be 100% under automated test.

--- src/main/java/com/example/automated_testing/Rectangle.java
+++ src/main/java/com/example/automated_testing/Rectangle.java
@@ -4,39 +4,64 @@ public class Rectangle {
     private double width, height;
 
     public Rectangle(double width, double height) {
-        this.width = width;
-        this.height = height;
+        setHeight(height);
+        setWidth(width);
     }
 
     public double getWidth() {
         return width;
     }
 
+    /**
+     * The width must be greater than 0
+     * @param width
+     */
     public void setWidth(double width) {
-        this.width = width;
+        if(width > 0)
+            this.width = width;
+        else
+            throw new IllegalArgumentException("Width must be greater than 0");
     }
-
 
     public double getHeight() {
         return height;
     }
 
+    /**
+     * The height must be greater than 0
+     * @param height
+     */
     public void setHeight(double height) {
-        this.height = height;
+        if (height > 0)
+            this.height = height;
+        else
+            throw  new IllegalArgumentException("Height must be greater than 0");
     }
 
+    /**
+     * Returns true if the height and width are the same
+     * @return
+     */
     public boolean isSquare()
     {
-        return false;
+        return height == width;
     }
 
+    /**
+     * This method returns the area of the rectangle
+     * @return double representing the area
+     */
     public double getArea()
     {
-        return -1;
+        return height * width;
     }
 
+    /**
+     * This is the length of all the sides added up
+     * @return
+     */
     public double getPerimeter()
     {
-        return -2;
+        return 2*width + 2*height;
     }
 }
--- src/main/tests/com/example/automated_testing/RectangleTest.java
+++ src/main/tests/com/example/automated_testing/RectangleTest.java
@@ -1,5 +1,6 @@
 package com.example.automated_testing;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,27 +8,117 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RectangleTest {
 
+    private Rectangle rectangle;
+    private Rectangle square;
+
     @BeforeEach
     void setUp() {
+        rectangle = new Rectangle(30,20);
+        square = new Rectangle(20,20);
     }
 
     @Test
     void setWidth() {
+        square.setWidth(100);
+        assertEquals(100, square.getWidth());
+    }
+
+    @Test
+    void setWidthInvalid0() {
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            square.setWidth(0);
+        });
+    }
+
+    @Test
+    void setWidthInvalidNegative() {
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            square.setWidth(-5);
+        });
     }
 
     @Test
     void setHeight() {
+        square.setHeight(100);
+        assertEquals(100, square.getHeight());
+    }
+
+    @Test
+    void setHeightInvalid0() {
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            square.setHeight(0);
+        });
+    }
+
+    @Test
+    void setHeightInvalidNegative() {
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            square.setHeight(-5);
+        });
+    }
+
+    @Test
+    void setHeightRectangle() {
+        rectangle.setHeight(100);
+        assertEquals(100, rectangle.getHeight());
+    }
+
+    @Test
+    void setHeightInvalid0Rectangle() {
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            rectangle.setHeight(0);
+        });
+    }
+
+    @Test
+    void setHeightInvalidNegativeRectangle() {
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            rectangle.setHeight(-5);
+        });
     }
 
     @Test
     void isSquare() {
+        assertTrue(square.isSquare());
+    }
+
+    @Test
+    void isSquareRectangle() {
+        assertFalse(rectangle.isSquare());
     }
 
     @Test
     void getArea() {
+        assertEquals(400, square.getArea());
+    }
+
+    @Test
+    void getAreaRectangle() {
+        assertEquals(600, rectangle.getArea());
     }
 
     @Test
     void getPerimeter() {
+        assertEquals(80, square.getPerimeter());
+    }
+    @Test
+    void getPerimeterRectangle() {
+        assertEquals(100, rectangle.getPerimeter());
+    }
+
+    @Test
+    void constructorInvalidHeight()
+    {
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            new Rectangle(10,0);
+        });
+    }
+
+    @Test
+    void constructorInvalidWidth()
+    {
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> {
+            new Rectangle(0,10);
+        });
     }
 }
